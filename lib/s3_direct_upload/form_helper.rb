7300fb118e1af3db9f6e4564e81f4080dd183d8f
Wil Helser <wil@wilhelser.com>
2012-10-13 00:09:31 -0500

    fixed expiration

--- lib/s3_direct_upload/form_helper.rb
+++ lib/s3_direct_upload/form_helper.rb
@@ -16,7 +16,7 @@ module S3DirectUpload
           aws_secret_access_key: S3DirectUpload.config.secret_access_key,
           bucket: S3DirectUpload.config.bucket,
           acl: "public-read",
-          expiration: 10.hours.from_now,
+          expiration: "2100-01-01T00:00:00Z",
           max_file_size: 500.megabytes,
           as: "file",
           key: key
